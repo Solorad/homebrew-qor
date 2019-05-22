68747945dec0159a8b3bfdbc3debc64d651f654c
Solorad <yarad8613@gmail.com>
2019-05-22 05:25:34 +0200

    Update homebrew repo

--- qor.rb
+++ qor.rb
@@ -1,8 +1,8 @@
 class Qor < Formula
   desc "Cli v2 (using Go)"
   homepage "https://github.com/Qordobacode/Cli-v2"
-  url "https://github.com/Qordobacode/Cli-v2/archive/version-0.2.tar.gz"
-  sha256 "6e5f4e1a1a78c22a191101fd99259d0bbec0edde904b164e38edec129b8caaf4"
+  url "https://github.com/Qordobacode/Cli-v2/archive/version-0.3.tar.gz"
+  sha256 "12e182d1cc0279aed6088d38f7a1102ca379048e47e7eaa5726d85a3ad5b2ad8"
   head "https://github.com/Qordobacode/Cli-v2.git"
 
   depends_on "go" => :build
@@ -11,8 +11,13 @@ class Qor < Formula
     ENV["GOPATH"] = buildpath
     src = buildpath/"src/github.com/qordobacode/cli-v2"
     src.install buildpath.children
+    ldflags = %W[
+        -s -w
+        -X github.com/CircleCI-Public/circleci-cli/cmd.PackageManager=homebrew
+        -X github.com/Qordobacode/Cli-v2/version.Version=0.3
+    ]
     src.cd do
-      system "go", "build", "-o", "#{bin}/qor"
+      system "go", "build", "-ldflags", ldflags.join(" "), "-o", "#{bin}/qor"
       prefix.install_metafiles
     end
   end
