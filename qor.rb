22757840ef72511f03ab05300bf3d410e78481ea
Evgenii Morenkov <yarad8613@gmail.com>
2019-07-10 01:35:48 +0200

    Update qor formula to v0.6.2

--- qor.rb
+++ qor.rb
@@ -1,8 +1,8 @@
 class Qor < Formula
   desc "Cli v2 (using Go)"
   homepage "https://github.com/Qordobacode/Cli-v2"
-  url "https://github.com/Qordobacode/Cli-v2/archive/v0.6.1.tar.gz"
-  sha256 "536be68909eef90b8fc6f085ac8c36a3cfc7e4eca1cd5853132537ad501c7ab8"
+  url "https://github.com/Qordobacode/Cli-v2/archive/v0.6.2.tar.gz"
+  sha256 "65113210d5f28fdb24279b04a76d0f45670ad339262b08f05591359faa0778cf"
   head "https://github.com/Qordobacode/Cli-v2.git"
 
   depends_on "go" => :build
@@ -14,7 +14,7 @@ class Qor < Formula
     ldflags = %W[
         -s -w
         -X github.com/CircleCI-Public/circleci-cli/cmd.PackageManager=homebrew
-        -X github.com/Qordobacode/Cli-v2/version.Version=0.6.1
+        -X github.com/Qordobacode/Cli-v2/version.Version=0.6.2
     ]
     src.cd do
       system "go", "build", "-ldflags", ldflags.join(" "), "-o", "#{bin}/qor"
