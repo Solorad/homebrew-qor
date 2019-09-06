f5dfcaa0f0c5526f1bbaeb67517a374371404954
Evgenii Morenkov <yarad8613@gmail.com>
2019-09-07 01:29:16 +0200

    qor.rb

--- qor.rb
+++ qor.rb
@@ -1,7 +1,7 @@
 class Qor < Formula
   desc "Cli v2 (using Go)"
   homepage "https://github.com/Qordobacode/Cli-v2"
-  url "https://github.com/Qordobacode/Cli-v2/archive/v0.7.1.tar.gz"
+  url "https://github.com/Qordobacode/Cli-v2/archive/v0.7.2.tar.gz"
   sha256 "9115f6e0382bd356ad12b05f8aded32799bb375bad90ac2749a95ad6fc1feb15"
   head "https://github.com/Qordobacode/Cli-v2.git"
 
@@ -13,8 +13,7 @@ class Qor < Formula
     src.install buildpath.children
     ldflags = %W[
         -s -w
-        -X github.com/CircleCI-Public/circleci-cli/cmd.PackageManager=homebrew
-        -X github.com/Qordobacode/Cli-v2/version.Version=0.7.1
+        -Xgithub.com/qordobacode/cli-v2/cmd/version.version=0.7.2
     ]
     src.cd do
       system "go", "build", "-ldflags", ldflags.join(" "), "-o", "#{bin}/qor"
